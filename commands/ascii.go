be1c1c1207f37036b0eb0dab6b883fceb5affa25
Matt Redfield <matt@secondspectrum.com>
2015-12-07 09:51:34 -0800

    added ascii

---
+++ commands/ascii.go
@@ -0,0 +1,18 @@
+package commands
+
+import "git.2nd.io/matt/lmdb-cli/core"
+
+var ascii = []byte(`____________  __________________       ______________________
+___  /___   |/  /__  __ \__  __ )      __  ____/__  /____  _/
+__  / __  /|_/ /__  / / /_  __  |_______  /    __  /  __  /
+_  /___  /  / / _  /_/ /_  /_/ /_/_____/ /___  _  /____/ /
+/_____/_/  /_/  /_____/ /_____/        \____/  /_____/___/
+`)
+
+type Ascii struct {
+}
+
+func (cmd Ascii) Execute(context *core.Context, input []byte) (err error) {
+	context.Output(ascii)
+	return nil
+}
